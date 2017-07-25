0a916f5a3cd1309804fd8ea150a192113b009a49
morsh <mor.shemesh@gmail.com>
2017-07-25 12:21:06 +0300

    fixing server function defs

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 	
-app.post('/analyze', function(req, res) {
+app.post('/analyze', (req, res) => {
     //var dataInput = [req.body.filename];
 });
 
-io.on('connection', function(socket){
+io.on('connection', socket => {
   console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 var port = process.env.PORT || 3000;
-http.listen(port, function() {
+http.listen(port, () => {
   console.log('Drone 4 good demo app listening on port 3000')
 });
