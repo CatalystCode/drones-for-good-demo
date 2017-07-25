807fc24987a9c3c2013e50b80862d832f7a97984
Rotem Dvir <t-rodvir@microsoft.com>
2017-07-25 11:42:02 +0300

    removing redundant code

--- server.js
+++ server.js
@@ -10,24 +10,6 @@ app.use(express.static('public'))
     });
 	
 app.post('/analyze', function(req, res) {
-    // For now just get an array from the client..
-    var dataInput = [req.body.payload];
-    model.eval(dataInput, (err, evalResult) =>{
-        if (err) {
-            console.info('Error during eval:', err);
-            res.end(err.toString(), 400);
-            return;
-        }
-
-        // so we use argmax to get the index of the highest value
-        var digit = cntk.utils.argmax(evalResult.output[0]);
-        responseBody = JSON.stringify({
-            'digit' : digit
-        });
-
-        console.info('Request processed, recognized digit is: ', digit)
-
-        res.end(responseBody , 200);
-    });
+    var dataInput = [req.body.filename];
+);
 });
-
